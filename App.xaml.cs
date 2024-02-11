1bc207fc27c36d6099eee70131ed4fb8da6c33bc
Cnmhqwerty <68733738+cnmhqwerty@users.noreply.github.com>
2024-02-11 09:46:10 +0000

    Project initialisation

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CrossPath
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
