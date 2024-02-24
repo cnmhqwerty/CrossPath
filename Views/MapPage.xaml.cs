2897752d0eb0a82f815382ef80543b075315ba9c
Cnmhqwerty <68733738+cnmhqwerty@users.noreply.github.com>
2024-02-24 14:13:04 +0000

    Live User Location

--- Views/MapPage.xaml.cs
+++ Views/MapPage.xaml.cs
@@ -1,4 +1,9 @@
+using Map = Microsoft.Maui.Controls.Maps.Map;
+using Microsoft.Maui.Controls.Maps;
+using System.Diagnostics;
 using Microsoft.Maui.Maps;
+using GeolocatorPlugin.Abstractions;
+using GeolocatorPlugin;
 
 namespace CrossPath.Views;
 
@@ -7,10 +12,67 @@ public partial class MapPage : ContentPage
 	public MapPage()
 	{
 		InitializeComponent();
+        Position pos = GetCurrentPosition().Result;
+        Location location = new Location(pos.Latitude, pos.Longitude);
+        MapSpan mapSpan = new MapSpan(location, 0.1, 0.1);
 
-        var location = new Location(53.47334804386584, -2.297137864535083);
-        var mapSpan = new MapSpan(location, 0.01, 0.01);
+        Map map = new Map
+        {
+            IsShowingUser = true
+        };
 
+        Content = map;
         map.MoveToRegion(mapSpan);
+
+        Pin pin = new Pin
+        {
+            Label = "Santa Cruz",
+            Address = "The city with a boardwalk",
+            Type = PinType.Place,
+            Location = new Location(36.9628066, -122.0194722)
+        };
+        map.Pins.Add(pin);
+    }
+
+    public static async Task<Position> GetCurrentPosition()
+    {
+        Position position = null;
+        try
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 100;
+
+            position = await locator.GetLastKnownLocationAsync();
+
+            if (position != null)
+            {
+                //got a cahched position, so let's use it.
+                return position;
+            }
+
+            if (!locator.IsGeolocationAvailable || !locator.IsGeolocationEnabled)
+            {
+                //not available or enabled
+                return null;
+            }
+
+            position = await locator.GetPositionAsync(TimeSpan.FromSeconds(20), null, true);
+
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine("Unable to get location: " + ex);
+        }
+
+        if (position == null)
+            return null;
+
+        var output = string.Format("Time: {0} \nLat: {1} \nLong: {2} \nAltitude: {3} \nAltitude Accuracy: {4} \nAccuracy: {5} \nHeading: {6} \nSpeed: {7}",
+                position.Timestamp, position.Latitude, position.Longitude,
+                position.Altitude, position.AltitudeAccuracy, position.Accuracy, position.Heading, position.Speed);
+
+        Debug.WriteLine(output);
+
+        return position;
     }
 }
